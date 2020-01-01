91433e0fb515de8c01a4490dcc69cb5d667f656d
choppa2 <falling_snowdin@protonmail.com>
2020-01-01 16:08:13 +0700

    This version finally works on Linux for some reason. Well, don't fix it if it ain't broke.

--- parser.py
+++ parser.py
@@ -1,12 +1,12 @@
 
 import json
 import os
-input_script_name = "script"
+input_script_name = "script.txt"
 out_script_name = "script"
 out_script = 0
 node = [{  # node[0]['nodes']
     "characters": [],
-    "nodes": [{"next": "1","node_name": "START","node_type": "start"}],
+    "nodes": [{"next": "1", "node_name": "START", "node_type": "start"}],
     "languages": ['ENG'],
     "variables": {},
     "editor_version": "2.1"
@@ -28,10 +28,9 @@ with open(input_script_name, 'r') as script:
     if ('NAME' in first_line):
 
         out_script_name = '%s.json' % first_line[1].strip()
-##        with open('%s.json' % first_line[1].strip(), 'w') as out_script:
+# with open('%s.json' % first_line[1].strip(), 'w') as out_script:
 ##                json.dump(template, out_script)
     for line in script:
-
 
         if "execute" in line.split(":")[0]:
             current_node = {
@@ -48,34 +47,33 @@ with open(input_script_name, 'r') as script:
                 current_node["next"] = None
             node[0]["nodes"].append(current_node.copy())
 ##
-##        elif "condition_branch" in in line.split(":")[0]:
-##            current_node = {
-##                "node_name": str(current_node_index),
-##                "node_type": "set_local_variable",
-##                "branches": {
-##                    "False": line.split(":")[1].split(",")[1],
-##                    "True": line.split(":")[1].split(",")[2]
-##                },
-##                "text": line.split(":")[1].split(",")[0]
-##            }
+# elif "condition_branch" in in line.split(":")[0]:
+# current_node = {
+# "node_name": str(current_node_index),
+# "node_type": "set_local_variable",
+# "branches": {
+# "False": line.split(":")[1].split(",")[1],
+# "True": line.split(":")[1].split(",")[2]
+# },
+# "text": line.split(":")[1].split(",")[0]
+# }
 ##
-##            node[0]["nodes"].append(current_node.copy())
+# node[0]["nodes"].append(current_node.copy())
 
-
-	elif "wait" in line.split(":")[0]:
-	    current_node = {
-		"time": float(line.split(":")[1].strip()),
-		"node_name": str(current_node_index),
-		"node_type": "wait",
-		"next": str(current_node_index+1)
-	    }
-	    if ("final" not in line.split(":")[0]):
-		current_node_index += 1
-		current_node["next"] = str(current_node_index)
-	    elif ("final" in line.split(":")[0]):
-		final_found_flag = True
-		current_node["next"] = None
-	    node[0]["nodes"].append(current_node.copy())
+        elif "wait" in line.split(":")[0]:
+            current_node = {
+                "time": float(line.split(":")[1].strip()),
+                "node_name": str(current_node_index),
+                "node_type": "wait",
+                "next": str(current_node_index+1)
+            }
+            if ("final" not in line.split(":")[0]):
+                current_node_index += 1
+                current_node["next"] = str(current_node_index)
+            elif ("final" in line.split(":")[0]):
+                final_found_flag = True
+                current_node["next"] = None
+            node[0]["nodes"].append(current_node.copy())
 
         elif "set" in line.split(":")[0]:
             current_node = {
@@ -111,23 +109,24 @@ with open(input_script_name, 'r') as script:
                 var_type = 2
             # check for variable name and variable value
 
-
-            print ('awthohou')
-            print (line.split(":")[1].split(",")[1].strip().upper())
+            print('awthohou')
+            print(line.split(":")[1].split(",")[1].strip().upper())
             if line.split(":")[1].split(",")[1].strip().upper() == "TRUE":
                 current_node["value"] = True
             elif line.split(":")[1].split(",")[1].strip().upper() == "FALSE":
                 current_node["value"] = False
-            elif line.split(":")[1].split(",")[1].strip().isdigit() == True: # isdigit only works when the number is positive ### IMPORTANT ###
-                current_node["value"] = int(line.split(":")[1].split(",")[1].strip())
+            # isdigit only works when the number is positive ### IMPORTANT ###
+            elif line.split(":")[1].split(",")[1].strip().isdigit() == True:
+                current_node["value"] = int(
+                    line.split(":")[1].split(",")[1].strip())
 
             if current_node["var_name"] not in node[0]["variables"]:
                 node[0]["variables"][current_node["var_name"]] = {
                     "type": var_type,
                     "value": current_node["value"]
                 }
-            print ("set for")
-            print (current_node)
+            print("set for")
+            print(current_node)
             # check for final tag
             if ("final" not in line.split(":")[0]):
                 current_node_index += 1
@@ -211,8 +210,6 @@ with open(input_script_name, 'r') as script:
 
             # append current_node the the out_script node object
             node[0]["nodes"].append(current_node.copy())
-
-
 
     if (final_found_flag):
         with open(out_script_name, "w") as out_script:
